102f69a5710b0d460a6f0199af254e9b38e29e62
Mark Kahn <cwolves@gmail.com>
2011-05-25 13:52:30 -0700

    Making config a .js instead of .json and adding various server profiles

---
+++ config/index.js
@@ -0,0 +1,82 @@
+var CL = require('cassandra').ConsistencyLevel;
+
+var servers = {
+	default : {
+		http_port  : 80,
+		https_port : 443,
+		debug      : false,
+
+		compress : {
+			js   : true,
+			html : true,
+			css  : true
+		},
+
+		database : {
+			keyspace    : 'Vouchr',
+			consistency : {
+				write : CL.QUORUM, // in a local environment QUORUM is identical to ONE, so this should never need to change
+				read  : CL.QUORUM
+			}
+		}
+	},
+
+	local_development : {
+		http_port  : 8001,
+		https_port : 8002,
+		debug      : true,
+		host       : 'vouchr.local',
+
+		compress : {
+			js   : false,
+			html : false,
+			css  : false
+		},
+
+		database : {
+			host : 'localhost',
+			port : 9160
+		}
+	},
+
+	development : {
+		debug : true,
+		host  : 'dev.vouchr.com',
+
+		database : {
+			keyspace : 'VouchrDev'
+		}
+	},
+
+	production : {
+		host : 'vouchr.com'
+	},
+
+
+	// this will extend from local_development server config
+	local_development_testing : {
+		database : {
+			keyspace : 'VouchrTesting'
+		}
+	},
+
+	// this will extend from development server config
+	development_testing : {
+		database : {
+			keyspace : 'VouchrDevTesting'
+		}
+	}
+
+	// this will extend from production server config
+	production_testing : {
+		database : {
+			keyspace : 'VouchrTesting'
+		}
+	}
+};
+
+servers.local_development_testing = $.extend(true, servers.local_development_testing, servers.local_development);
+servers.development_testing       = $.extend(true, servers.development_testing      , servers.local_development);
+servers.production_testing        = $.extend(true, servers.production_testing       , servers.local_development);
+
+module.exports = $.extend(true, {}, servers.default, servers[process.env.NODE_ENV]);
